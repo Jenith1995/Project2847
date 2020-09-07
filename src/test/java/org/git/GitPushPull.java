6575c153a2e0774d779056c27403e9d8ab88e6b7
Jenith1995 <jeni.28347@gmail.com>
2020-09-07 10:31:24 -0700

    Done

--- src/test/java/org/git/GitPushPull.java
+++ src/test/java/org/git/GitPushPull.java
@@ -5,6 +5,10 @@ public class GitPushPull {
 		String s1 = "jeni";
 		String s2 = s1;
 		s1="Ammu";
+		int length = s1.length();
+		System.out.println(length);
+		boolean contains = s1.contains("E");
+		System.out.println(s1);
 	}
 
 }
