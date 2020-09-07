64ff89996f9bab00d70a93c11fcd3b0b2ec73d6e
Jenith1995 <jeni.28347@gmail.com>
2020-09-07 10:24:15 -0700

    To J

--- src/test/java/org/git/GitPushPull.java
+++ src/test/java/org/git/GitPushPull.java
@@ -5,6 +5,10 @@ public class GitPushPull {
 		String s1 = "jeni";
 		String s2 = s1;
 		s1="Ammu";
+		System.out.println(s1);
+		char charAt = s1.charAt(2);
+		String concat = s1.concat(s2);
+		System.out.println(concat);
 	}
 
 }
